6c5601b634cca76f31212f02e6b8ae3260ad8cf6
SiraDuryan <sira.duryan@gmail.com>
2023-05-09 22:35:30 +0400

    Calculator

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import './index.scss';
+import { useState } from 'react';
+
+
+export default function App() {
+  const [valueA, setValueA] = useState("");
+  const [valueB, setValueB] = useState("");
+  const [result, setResult] = useState("");
+  const [action, setAction] = useState(0);
+
+  const submit =() => {
+    if (action==="+") {
+      setResult (parseInt(valueA) + parseInt(valueB))
+    }
+    if (action==="-") {
+      setResult (parseInt(valueA) - parseInt(valueB))
+      
+    }
+    if (action==="*") {
+      setResult (parseInt(valueA) * parseInt(valueB))
+      
+    }
+    if (action==="/") {
+      setResult (parseInt(valueA) / parseInt(valueB))
+      
+    }
+  }
+
+  const clear = () => {
+    setResult ("");
+    setValueA ("");
+    setValueB ("");
+
+  }
+
+  return (
+    <>
+    <div className='calculator'>
+    <div className='block1'>
+      <h1>Calculator</h1>
+      <input type="text" value={valueA} onChange= {(e)=>setValueA(e.target.value)}/>
+      <input type="text" value={valueB} onChange= {(e)=>setValueB(e.target.value)}/>
+      <button onClick={submit}>=</button>
+    </div>
+    <div className='block2'>
+      <button onClick={()=>setAction("+")}>+</button>
+      <button onClick={()=>setAction("-")}>-</button>
+      <button onClick={()=>setAction("*")}>*</button>
+      <button onClick={()=>setAction("/")}>/</button>
+    
+    </div>
+    <div className='result'>Result: {result}</div>
+    <button className='clear' onClick={clear} > Clear</button>
+    </div>
+    </>
+  );
+}
